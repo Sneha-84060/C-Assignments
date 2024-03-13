a01decc8ef7fe3cf3ae7ea7a79e550e4aa8200c3
1612sneha <162573921+1612sneha@users.noreply.github.com>
2024-03-13 22:00:27 +0530

    AS2-Q1.c

--- AS2-Q1.c
+++ AS2-Q1.c
@@ -3,10 +3,10 @@ int main()
 {
 int num1, num2;
 printf("Enter num1:");
-scanf("Enter num1:");
+scanf("%d",&num1);
 
 printf("Enter num2:");
-scanf("Enter num2:");
+scanf("%d",&num2);
 
 if(num2!=0)
   printf("Division = %.2d\n",num1/num2);
